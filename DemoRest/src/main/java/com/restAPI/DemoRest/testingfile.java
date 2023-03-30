3e2a024bd02780adc18620832ed26287e8b449e0
rajasekar <rajasekar.19@gmail.com>
2023-03-30 15:36:25 +0530

    return violation

--- DemoRest/src/main/java/com/restAPI/DemoRest/testingfile.java
+++ DemoRest/src/main/java/com/restAPI/DemoRest/testingfile.java
@@ -1,6 +1,29 @@
 package com.restAPI.DemoRest;
 class testingClass{
+    public int returnMethodCount(int count){
+        if(count==1){
+            if(true){
+                System.out.println(count);
+                return 1;
+            }
+            else if(count==2){
+                return 2;
+            }
+            else if(count==3){
+                return 3;
+            }
+            else if(count==4){
+                return 4;
+            }
+            else{
+                return -9;
+            }
+        }
+     return 0;
+    }
     public static void main(String[] param){
         System.out.println("Testing Class");
+        testingClass test = new testingClass();
+        test.returnMethodCount(1);
     }
 }
